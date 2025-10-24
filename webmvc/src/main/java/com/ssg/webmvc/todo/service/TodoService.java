1d19677dd0f0d353de01a4a4b086a97add79fae2
heathcliff4736 <lookout97@naver.com>
2025-10-24 10:59:15 +0900

    1024_1059

--- webmvc/src/main/java/com/ssg/webmvc/todo/service/TodoService.java
+++ webmvc/src/main/java/com/ssg/webmvc/todo/service/TodoService.java
@@ -4,6 +4,7 @@ import com.ssg.webmvc.todo.dao.TodoDAO;
 import com.ssg.webmvc.todo.domain.TodoVO;
 import com.ssg.webmvc.todo.dto.TodoDTO;
 import com.ssg.webmvc.todo.util.MapperUtil;
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 
 import java.time.LocalDate;
@@ -11,13 +12,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-// 객체의 개수를 결정할 때 사용한다.
-// 현재 INSTANCE가 한 개 이므로
-// 컨트롤러가 접근할 수 있는 서비스객체는 1개만 사용하겠다. 라는 의미
-// 접근시 : TodoService.INSTANCE
-// 객체를 하나만 생성해서 사용 (singletone pattern)
-// 여러 컨트롤러들이 TodoService 객체를 통해서 원하는 데이터를 주고받는 구조로 구성
+@Log4j2
 public enum TodoService {
+
+    // 객체의 개수를 결정할 때 사용한다.
+    // 현재 INSTANCE가 한 개 이므로
+    // 컨트롤러가 접근할 수 있는 서비스객체는 1개만 사용하겠다. 라는 의미
+    // 접근시 : TodoService.INSTANCE
+    // 객체를 하나만 생성해서 사용 (singletone pattern)
+    // 여러 컨트롤러들이 TodoService 객체를 통해서 원하는 데이터를 주고받는 구조로 구성
     INSTANCE;
 
     // 인젝션 의존성 주입 (DI)
@@ -31,7 +34,8 @@ public enum TodoService {
 
     public void register(TodoDTO todoDTO) throws Exception {
         TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
-        System.out.println("todo VO :" + todoVO);
+//        System.out.println("todo VO :" + todoVO);
+        log.info(todoVO);
         dao.insert(todoVO);
         // register() TodoDTO 파라미터를 받고, ModelMapper를 통해서 TodoVO 객체로 변환을 한 후
         // dao.insert(todoVO)를 통해 TodoVO를 객체를 전달하며 등록기능을 요청.
