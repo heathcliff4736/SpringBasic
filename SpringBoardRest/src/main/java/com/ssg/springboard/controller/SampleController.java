a3ec9790dc85739fa8ee1864d7bcc72ff0820d3d
heathcliff4736 <lookout97@naver.com>
2025-11-03 12:22:55 +0900

    1103_1222

---
+++ SpringBoardRest/src/main/java/com/ssg/springboard/controller/SampleController.java
@@ -0,0 +1,13 @@
+package com.ssg.springboard.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+// URL 작업부터
+@RequestMapping("/sample")
+public class SampleController {
+    // url: /basic get방식 요청 구현
+}
